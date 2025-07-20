1de6bbb3379a649cb23df62e59f6eeee35d311d2
gownikranthi <gownikranthi2000@gmail.com>
2025-07-20 22:32:32 +0530

    fix: return raw socket instance from useSocket hook for correct event handling in LivePoll and other components

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,53 +1,20 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useRef } from 'react';
 import socketService from '../services/socket';
-import { updateCurrentEvent, updatePoll, addQuestion } from '../redux/slices/eventSlice';
-import type { Event, Poll, Question } from '../services/events';
 
 export const useSocket = (eventId?: string) => {
-  const dispatch = useDispatch();
+  const socketRef = useRef(socketService.connect());
 
   useEffect(() => {
-    // Connect socket
-    const socket = socketService.connect();
-
-    // Join session if eventId provided
     if (eventId) {
       socketService.joinSession(eventId);
     }
-
-    // Set up listeners
-    socketService.onSessionUpdate((data: Event) => {
-      dispatch(updateCurrentEvent(data));
-    });
-
-    socketService.onPollUpdate((data: Poll) => {
-      dispatch(updatePoll(data));
-    });
-
-    socketService.onNewQuestion((data: Question) => {
-      dispatch(addQuestion(data));
-    });
-
-    // Cleanup function
     return () => {
       socketService.removeAllListeners();
       if (!eventId) {
         socketService.disconnect();
       }
     };
-  }, [eventId, dispatch]);
+  }, [eventId]);
 
-  return {
-    submitPoll: (pollId: string, answer: string) => {
-      if (eventId) {
-        socketService.submitPoll(eventId, pollId, answer);
-      }
-    },
-    submitQuestion: (question: string) => {
-      if (eventId) {
-        socketService.submitQuestion(eventId, question);
-      }
-    },
-  };
+  return socketRef.current;
 };
